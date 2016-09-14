feeb22e7469b2efad537375437b63634fd465841
wlsf82 <wlsf82@gmail.com>
2016-09-14 23:29:44 +0200

    Created new test to use faker.lorem functions.

--- spec.js
+++ spec.js
@@ -21,3 +21,21 @@ describe('Choko - Create account', () => {
     expect(element(by.repeater('error in errors')).getText()).toEqual('Passwords must match.');
   });
 });
+
+describe('Choko - Blog', () => {
+  it('try to create blog post without permission', () => {
+    const ramdonName = faker.lorem.word();
+    const randomTitle = faker.lorem.words();
+    const randomText = faker.lorem.sentences();
+
+    browser.get('create/blog');
+
+    element(by.id('element-type-blog-name')).sendKeys(ramdonName);
+    element(by.id('element-type-blog-title')).sendKeys(randomTitle);
+    element(by.className('note-editable')).sendKeys(randomText);
+    element(by.id('element-type-blog-submit')).click();
+
+    expect(element(by.repeater('error in errors')).isDisplayed()).toBe(true);
+    expect(element.all(by.repeater('error in errors')).getText()).toContain('You don\'t have permission to access this page.');
+  });
+});
