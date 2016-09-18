8f72906efac1cfa860cb6bea554f97ed84a0bdf0
raulovidiu <raulhuluban@gmail.com>
2016-09-18 06:53:32 +0300

    Created Page Objects directory. Refactored Create Blog Post using PO

---
+++ page-Objects/create-blog-post.pageObject.js
@@ -0,0 +1,14 @@
+"use strict";
+
+class CreateBlogPost {
+
+	constructor() {
+		this.name = element(by.id('element-type-blog-name'));
+		this.title = element(by.id('element-type-blog-title'));
+		this.body = element(by.className('note-editable'));
+		this.save = element(by.id('element-type-blog-submit'));
+	}
+
+};
+
+module.exports = CreateBlogPost;
