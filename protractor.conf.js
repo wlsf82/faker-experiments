ad05e86ed127cc57d4e185b66d25324e8afd7091
raulovidiu <raulhuluban@gmail.com>
2016-09-19 09:26:16 +0300

    Disabling Browser Extensions that caused false negatives

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,15 +1,20 @@
 'use strict';
 
 module.exports.config = {
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+
   directConnect: true,
 
   baseUrl: 'http://choko.org/',
 
   specs: ['spec.js'],
 
-  capabilities: { 'browserName': 'chrome' },
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {'args': ['--disable-extensions']}
+  },
 
   onPrepare() {
-  	browser.driver.manage().window().maximize();
+    browser.driver.manage().window().maximize();
   }
 };
