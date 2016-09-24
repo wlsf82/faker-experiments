c140b36940d312808be682bd7eef2e676b2ab83a
wlsf82 <wlsf82@gmail.com>
2016-09-25 01:28:31 +0200

    Uncommented seleniumAddress confguration. When directConnect is tru this is not considered, so there is not need to comment it.

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,7 +3,7 @@
 const SpecReporter = require('jasmine-spec-reporter');
 
 module.exports.config = {
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
 
   directConnect: true,
 
