cdddc6803cb44d307f2f322887d7d7db7b9d71fe
Walmyr Filho <walmyr@appear.in>
2016-09-26 21:06:27 +0200

    Added new test case and refatoring already existing test with function for adding item in the todo list.

--- page-objects/todo-mvc.po.js
+++ page-objects/todo-mvc.po.js
@@ -5,6 +5,13 @@ class TodoMvc {
   constructor() {
     this.newTodoField = element(by.model('newTodo'));
     this.todoList = element(by.id('todo-list'));
+    this.toggleAll = element(by.id('toggle-all'));
+    this.clearCompleted = element(by.id('clear-completed'));
+  }
+
+  addItemOnList(item) {
+    this.newTodoField.sendKeys(item);
+    this.newTodoField.sendKeys(protractor.Key.ENTER);
   }
 
   visit() {
--- specs/todo-mvc.spec.js
+++ specs/todo-mvc.spec.js
@@ -6,14 +6,25 @@ const TodoMvc = require('../page-objects/todo-mvc.po');
 describe('TODO MVC', () => {
   const todoMvc = new TodoMvc();
 
+  beforeEach(() => {
+    todoMvc.visit();
+  })
+
   it('add item on TODO list', () => {
     const randomWord = faker.random.word();
 
-    todoMvc.visit();
-
-    todoMvc.newTodoField.sendKeys(randomWord);
-    todoMvc.newTodoField.sendKeys(protractor.Key.ENTER);
+    todoMvc.addItemOnList(randomWord);
 
     expect(todoMvc.todoList.getText()).toContain(randomWord);
+  });
+
+  it('complete TODO items and check that list is empty', () => {
+      const randomWord = faker.random.word();
+
+      todoMvc.addItemOnList(randomWord);
+      todoMvc.toggleAll.click();
+      todoMvc.clearCompleted.click();
+
+      expect(todoMvc.todoList.isDisplayed()).not.toBe(true);
   });
 });
